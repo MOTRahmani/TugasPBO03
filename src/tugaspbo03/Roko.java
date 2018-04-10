98584df64d2c9653aa9b5c27f241fbea10542f73
Muhammad Rahmani <muhammadrahmani84@gmail.com>
2018-04-10 19:15:48 +0800

    buat getter and setter

--- src/tugaspbo03/Roko.java
+++ src/tugaspbo03/Roko.java
@@ -4,6 +4,30 @@ public class Roko {
     private String merk;
     private double nikotin;
     private double harga;
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public double getNikotin() {
+        return nikotin;
+    }
+
+    public void setNikotin(double nikotin) {
+        this.nikotin = nikotin;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
     
     void cetakInfo(){
         System.out.println("Merk \t:"+merk+"\n"+
