a302645c110170836895bf097905ff42e07239b8
Muhammad Rahmani <muhammadrahmani84@gmail.com>
2018-04-10 19:07:48 +0800

    Membuat method cetakInfo()

--- src/tugaspbo03/Roko.java
+++ src/tugaspbo03/Roko.java
@@ -4,4 +4,10 @@ public class Roko {
     String merk;
     double nikotin;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t:"+merk+"\n"+
+                "Takaran \t:"+nikotin+"\n"+
+                "Harga \t:"+harga);
+    }
 }
