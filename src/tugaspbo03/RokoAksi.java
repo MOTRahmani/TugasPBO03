121c5dcb88ef960238376add8061f5e1bc658646
Muhammad Rahmani <muhammadrahmani84@gmail.com>
2018-04-10 19:17:46 +0800

    menggunakan method set

--- src/tugaspbo03/RokoAksi.java
+++ src/tugaspbo03/RokoAksi.java
@@ -3,9 +3,9 @@ package tugaspbo03;
 public class RokoAksi {
     public static void main(String[] args) {
         Roko r1 = new Roko();
-        r1.merk="Malboro";
-        r1.nikotin=1.9;
-        r1.harga=20000;
+        r1.setMerk("LA");
+        r1.setNikotin(1.2);
+        r1.setHarga(16000);
         
         r1.cetakInfo();
     }
